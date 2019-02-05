40a5d2faf6234f1c19a0563b8e5a327da35af2f9
Romain Desgouilles <romain.desgouilles@gmail.com>
2019-02-05 15:54:39 +0100

    delete posts

--- admin.php
+++ admin.php
@@ -10,16 +10,15 @@ echo $_GET["message"];
 <?php
 
 // select all posts to db
-$req = $db->query('SELECT * FROM posts');
+$req = $db->query('SELECT id, title FROM posts');
 $req->execute();
 
-// while show all posts 
+
 while($row = $req->fetch(PDO::FETCH_ASSOC)){
-    echo "<h2>{$row["title"]}</h2>";
-    echo $_GET["<button aria-label="Close Account Info Modal Box">&times;</button>"];
+    echo "<h4>{$row["title"]}</h4>";
+    echo "<a href='delete_post.php?id={$row["id"]}'>X</a>";
 }
 
 ?>
-
 </body>
 </html>
