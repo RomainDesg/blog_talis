1c8e9f57ca7fc205cae70a8d36eb28399aba0109
Romain Desgouilles <romain.desgouilles@gmail.com>
2019-02-05 15:07:14 +0100

    set up posts on admin.php

--- admin.php
+++ admin.php
@@ -7,5 +7,19 @@ echo $_GET["message"];
 
     <a href="add_post.php">Ajouter article</a>
 
+<?php
+
+// select all posts to db
+$req = $db->query('SELECT * FROM posts');
+$req->execute();
+
+// while show all posts 
+while($row = $req->fetch(PDO::FETCH_ASSOC)){
+    echo "<h2>{$row["title"]}</h2>";
+    echo $_GET["<button aria-label="Close Account Info Modal Box">&times;</button>"];
+}
+
+?>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,7 +4,14 @@ require 'nav-bar.php';
 require 'database.php'; 
 
 // select all posts to db
+$req = $db->query('SELECT * FROM posts');
+$req->execute();
+
 // while show all posts 
+while($row = $req->fetch(PDO::FETCH_ASSOC)){
+    echo "<h2>{$row["title"]}</h2>";
+    echo "<p>{$row["content"]}</p>";
+}
 
 
 ?>
